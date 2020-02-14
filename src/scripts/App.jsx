0367dfa540d68d5822a55173723e50a1e6521e4e
lollipop-onl <lollipop@simoochee.net>
2020-02-15 00:24:04 +0900

    💄 update style

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -19,16 +19,40 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Hello world</h1>
-      <input type="search" onInput={onInput}/>
-      <ul>
+    <div className="page-container">
+      <h1 className="title">ゴミ分別ナビ for CHIBA</h1>
+      <p className="description">
+        千葉市のゴミ分別カタログです。
+        <a
+          href="https://www.city.chiba.jp/kankyo/junkan/shushugyomu/gomibunbetujiten.html"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link"
+        >データの出典元</a>
+      </p>
+      <div className="field">
+        <input className="input" placeholder="Keyword..." type="search" onInput={onInput}/>
+        <div className="count">{filteredData.length}件</div>
+      </div>
+      <ul className="list">
         {filteredData.map(({ name, separation, description}) => (
-          <div key={`${name}${separation}`}>
-            {name}, {separation}, {description}
-          </div>
+          <li className="data-catalog item" key={`${name}${separation}`}>
+            <div className="name">{name}</div>
+            <div className="separation">{separation}</div>
+            {description ? (
+              <div className="description">{description}</div>
+            ) : null}
+          </li>
         ))}
       </ul>
+      <a
+        href="https://www.lollipop.onl"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="footer"
+      >
+        Powered by lollipop.onl
+      </a>
     </div>
   );
 };
